47543ed27dfa344741d5025efb6b89fde0b79209
Stephen <stephen_h369@live.co.uk>
2020-08-26 15:28:30 +0100

    tests done for techStaff

--- src/test/java/DatabaseAdminTest.java
+++ src/test/java/DatabaseAdminTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.DatabaseAdmin;
+
+import static org.junit.Assert.assertEquals;
+
 public class DatabaseAdminTest {
+    DatabaseAdmin databaseAdmin;
+
+    @Before
+    public void before() {
+        databaseAdmin = new DatabaseAdmin("Bob", "FG 98 67 32 T", 25000.00);
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        databaseAdmin.raiseSalary(5000.00);
+        assertEquals(30000.00, databaseAdmin.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canPayBonus() {
+        databaseAdmin.payBonus();
+        assertEquals(25250.00, databaseAdmin.getSalary(), 0.01);
+    }
 }
--- src/test/java/DeveloperTest.java
+++ src/test/java/DeveloperTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeveloperTest {
+    Developer developer;
+
+    @Before
+    public void before() {
+        developer = new Developer("Jimmy", "CD 45 67 89 E", 30000.00);
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        developer.raiseSalary(5000.00);
+        assertEquals(35000.00, developer.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canPayBonus() {
+        developer.payBonus();
+        assertEquals(30300.00, developer.getSalary(), 0.01);
+    }
 }
